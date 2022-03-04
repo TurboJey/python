ca3cfbd0ce09aedbdd7b3150e734bfdb2e33880a
TurboJey <mpt.avkanakin@gmail.com>
2022-03-04 11:31:50 +0300

    add

--- bond.py
+++ bond.py
@@ -7,3 +7,5 @@ import pymysql
 import re
 from getpass import getpass
 
+interface = subprocess.check_output("ip -o link show | awk -F': ' '{print $2}' | grep 'enp[0-9]\|ens[0-9]' | uniq -w5 -D", shell=True) 
+print (interface)
