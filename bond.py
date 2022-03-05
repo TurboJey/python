c2d715179f0b872e1a03c9b2128a31542f569b09
TurboJey <mpt.avkanakin@gmail.com>
2022-03-05 10:28:01 +0300

    write conf network interface

--- bond.py
+++ bond.py
@@ -3,9 +3,25 @@ from readline import insert_text
 import subprocess
 import os
 import paramiko
-import pymysql
 import re
 from getpass import getpass
 
-interface = subprocess.check_output("ip -o link show | awk -F': ' '{print $2}' | grep 'enp[0-9]\|ens[0-9]' | uniq -w5 -D", shell=True) 
-print (interface)
+interface = subprocess.check_output("ip -o link show | awk -F': ' '{print $2}' | grep 'vmnet' | uniq -w5 -D", shell=True).decode('utf-8')
+interface = interface.split()
+if interface != "none":
+    i = 0
+    while i < len(interface):
+        subprocess.call("cat /dev/null > /home/kanakin/testbond/" + interface[0], shell=True)
+        subprocess.call("echo 'MTU = 9216' >> /home/kanakin/testbond/" + interface[i], shell=True)
+        subprocess.call("echo 'TYPE=Ethernet' >> /home/kanakin/testbond/" + interface[i], shell=True)
+        subprocess.call("echo 'PROXY_METHOD=none' >> /home/kanakin/testbond/" + interface[i], shell=True)
+        subprocess.call("echo 'BROWSER_ONLY=no' >> /home/kanakin/testbond/" + interface[i], shell=True)
+        subprocess.call("echo 'DEVICE='"+ interface[i] + ">> /home/kanakin/testbond/" + interface[i], shell=True)
+        subprocess.call("echo 'NAME=slave'" + str(i) + ">> /home/kanakin/testbond/" + interface[i], shell=True)
+        subprocess.call("echo 'BOOTPROTO=none' >> /home/kanakin/testbond/" + interface[i], shell=True)
+        subprocess.call("echo 'SLAVE=yes' >> /home/kanakin/testbond/" + interface[i], shell=True)
+        subprocess.call("echo 'NM_CONTROLLED=yes' >> /home/kanakin/testbond/" + interface[i], shell=True)
+        subprocess.call("echo 'ONBOOT=yes' >> /home/kanakin/testbond/" + interface[i], shell=True)
+         #print (interface[i])
+        i = i + 1
+    
