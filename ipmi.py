115427449db7b5c20b51ca6ae4a3bf6c36d27a2d
TurboJey <mpt.avkanakin@gmail.com>
2022-04-04 14:20:26 +0300

    fix

--- ipmi.py
+++ ipmi.py
@@ -1,16 +1,23 @@
+from asyncio.log import logger
 from asyncio.subprocess import PIPE
 import subprocess
 import re
 import os
 import sys
-import platform
+import logging, logging.config
 from sys import stdout
 import time
+from datetime import datetime
+
+
+logging.config.fileConfig('/home/user/python/loging.conf')
 
 
 def chanel():
-    i = 1
-    while i < 10:   
+    logger = logging.getLogger('chanel')
+    logger.debug(str(datetime.now()) + " - Script starting mode: chanel")
+    i = 9
+    while i > 0:   
         j = str(i)
         ipmi = subprocess.Popen("ipmitool lan print " + j, shell = True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
         streamdata = ipmi.communicate()[0]
@@ -22,30 +29,39 @@ def chanel():
             spli = ipmi.splitlines()
             for line in spli:
                 if re.match("IP Address  ", str(line)):
-                    print("ipmi use "+ j +" chanel")
+                    logger.info('Detect Chanel: '+ j)
                     return(spli)
-            i = i + 1
+            logger.debug('chanel '+ str(i) + ' not active')
+            i=i-1
+        elif rc == 1:
+            logger.debug('chanel '+ str(i) + ' not active')
+            i=i-1
         elif rc == 127:
-            print(rc)
-            print(streamdata)
-            #print('install ipmitool? y/n: ')
+            logger.debug(rc)
+            logger.info(streamdata)
             ins = (input('install ipmitool? y/n: '))
-            if ins == 'y' or ins == 'Y' or ins == '':
+            if ins == 'y' or ins == 'Y' or ins == '': 
                 if 'Debian' in str(pl):
+                    logger.debug('install ipmitool')
                     subprocess.Popen('apt install ipmitool', shell = True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                     time.sleep(3)
                     chanel()
             else:
+                logger.debug('user not install ipmitool')
                 print('goodbye')
                 sys.exit()
         else:
-            print(streamdata)
-            print(rc)
+            logger.info(streamdata)
+            logger.info(rc)
             sys.exit()
         
 
 
 class Ipmi:
+
+
+    logger = logging.getLogger('ipm')
+    logger.debug(str(datetime.now()) + " - Script starting mode: ipmi" )
 
 
     def __init__(self, spli):
@@ -54,10 +70,10 @@ class Ipmi:
         self._bmcdata = {}
         self.data_processing()
         self.bmc_data()
-        self.macadd = self.mac()
         self.ipadd = self.ip()
+        self.macadd = self.mac()
         self.bmc = self.bmc_ver()
-        
+
 
     def data_processing(self):
         for line in self.chanel:
@@ -77,14 +93,21 @@ class Ipmi:
     
 
     def ip(self):
+        logger = logging.getLogger('ipm')
+        #logger.debug(str(datetime.now()) + " - Script starting mode: ipmi" )
+        logger.debug('ip addres : ' + self._dictdata['IP Address'])
         return self._dictdata['IP Address']
 
 
     def mac(self):
+        logger = logging.getLogger('ipm')
+        logger.debug('mac addres : ' + self._dictdata['MAC Address'])
         return self._dictdata['MAC Address']
 
 
     def bmc_ver(self):
+        logger = logging.getLogger('ipm')
+        logger.debug('bmc version : ' + self._bmcdata['IPMI Version'])
         return self._bmcdata['IPMI Version']
 
     
@@ -92,4 +115,4 @@ ch = chanel()
 ipmi = Ipmi(ch)
 print(ipmi.ipadd)
 print(ipmi.macadd)
-print(ipmi.bmc)
+print(ipmi.bmc) 
